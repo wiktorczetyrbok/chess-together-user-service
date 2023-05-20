f0d6527bc02c67986a5b00d30818f8f98164d485
Wiktor Czetyrbok <kasztanators2701@gmail.com>
2023-05-20 14:41:52 +0200

    added extractor of active player

---
+++ src/main/java/com/myApp/web/game/logic/impl/ActivePieceExtractorImpl.java
@@ -0,0 +1,22 @@
+package com.myApp.web.game.logic.impl;
+
+import com.myApp.web.game.Square;
+import com.myApp.web.game.logic.ActivePieceExtractor;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component("activePieceExtractorImpl")
+public class ActivePieceExtractorImpl implements ActivePieceExtractor {
+    @Override
+    public List<Square> extractSquaresWithActivePieces(String activePlayer, List<Square> squares) {
+        List<Square> activeSquares= new ArrayList<>();
+        for(Square square : squares){
+            if(square.getPiece()!=null && square.getPiece().getOwner().equals(activePlayer)){
+                activeSquares.add(square);
+            }
+        }
+        return activeSquares;
+    }
+}
