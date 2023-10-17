5bc3dd7b6c4628016f68b37c8ababfb44cb2acd7
Wiktor <kasztanators2701@gmail.com>
2023-10-17 16:17:59 +0200

    Remove unecessart files from backend branch

--- game-service/src/main/java/chesstogether/chess/game/service/ChessGameService.java
+++
@@ -1,7 +0,0 @@
-package chesstogether.chess.game.service;
-
-import chesstogether.chess.game.Board;
-
-public interface ChessGameService {
-    Board getGameBoard(String x1, String y1, String x2, String y2);
-}
