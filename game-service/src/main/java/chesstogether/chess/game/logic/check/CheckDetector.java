a24ff6c03ab12bb5ee978ede435696f557cae9c5
Wiktor Czetyrbok <88346002+kasztanators@users.noreply.github.com>
2023-10-18 17:55:45 +0200

    Delete game-service directory

--- game-service/src/main/java/chesstogether/chess/game/logic/check/CheckDetector.java
+++
@@ -1,7 +0,0 @@
-package chesstogether.chess.game.logic.check;
-
-import chesstogether.chess.game.Board;
-
-public interface CheckDetector {
-    boolean detectCheck(Board board);
-}
--- game-service/src/main/java/chesstogether/chess/game/logic/moveCalc/MoveCalcFactory.java
+++
@@ -1,7 +0,0 @@
-package chesstogether.chess.game.logic.moveCalc;
-
-import chesstogether.chess.game.Piece;
-
-public interface MoveCalcFactory {
-    MoveCalc buildMoveCalculator(Piece piece);
-}
